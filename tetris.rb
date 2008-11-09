d1b37e3781d37c4f1a08b5cf06b18588a777b9e5
aprk <alan@rimmkaufman.com>
2008-11-09 11:30:26 -0500

    motion and falling

--- tetris.rb
+++ tetris.rb
@@ -19,11 +19,11 @@ background.fill(COLOR_BACKGROUND)
 background.blit(screen, [0, 0]) # clear screen
 
 
-shape = Shape.new(BLACK)
-shape.draw(screen)
-
 draw_screen_border(screen)
 screen.update
+
+shape = Shape.new(BLACK)
+shape.draw(screen)
 
 nav = {	K_RIGHT => {:state => false, :action => lambda { shape.right}, :repeat=>true},
   K_LEFT=> {:state => false, :action => lambda { shape.left}, :repeat => true},
@@ -32,6 +32,9 @@ K_1 => {:state => false, :action => lambda { shape.rotate_left}, :repeat=>false}
 
 game_over = false
 game_paused = false
+
+
+
 
 queue = Rubygame::EventQueue.new
 clock = Clock.new { |c| c.target_framerate = 30 }
@@ -43,14 +46,13 @@ until game_over do
     elsif e.kind_of?(KeyDownEvent) and  e.key == K_ESCAPE
       game_paused = !game_paused
     elsif e.kind_of?(KeyDownEvent) and e.key == K_SPACE
-      shape.fall
+      shape.delay_ms = 0
     elsif e.kind_of?(KeyDownEvent) and nav.has_key?(e.key) then
       nav[e.key][:state] = true
     elsif e.kind_of?(KeyUpEvent) and nav.has_key?(e.key) then
       nav[e.key][:state] = false
     end
   end
-
 
   nav.values.each do |a|
     a[:action].call if a[:state]
@@ -60,6 +62,7 @@ until game_over do
   clock.tick
   background.blit(screen, [0, 0]) # clear screen
   draw_screen_border(screen)
+  shape.fall
   shape.draw(screen)
   shape.draw_grid(screen)
   screen.update
